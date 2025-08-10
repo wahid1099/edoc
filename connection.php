9911eabc41df8856d03d299c3564990b94017e02
wahid1099 <wahidahmed890@gmail.com>
2025-08-10 19:47:15 +0600

    Initial commit of existing project

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";  
+$password = ""; 
+$dbname = "edoc"; 
+
+$database = new mysqli($servername, $username, $password, $dbname);
+
+if ($database->connect_error) {
+    die("Échec de la connexion : " . $database->connect_error);
+}
+?>
+
